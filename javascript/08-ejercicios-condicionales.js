35053d4e0d73c0301306d697762fd6e42bfc3311
cristianjmnz <cristianjimenez9595@gmail.com>
2026-07-10 01:06:44 +0200

    feat: resolucion de ejercicios

---
+++ javascript/08-ejercicios-condicionales.js
@@ -0,0 +1,47 @@
+// if/else/else if/ternaria
+
+// 1. Imprime por consola tu nombre si una variable toma su valor
+let x = 2
+if (x == 2){
+    console.log("Bienvenido Cristian")
+}
+
+// 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
+let user = "admin"
+let password = "admin1"
+
+if(user === "admin" && password === "admin1"){
+    console.log("Bienvenido Admin!")
+}
+
+// 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
+let num = 7;
+
+if (num !== 0){
+	if (num > 0){
+		console.log("EL numero es positivo");
+	}
+	else{
+		console.log("El numero es negativo");
+	}
+}
+else{
+	console.log("El numero es 0");
+}
+
+// 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
+
+// 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
+//    dependiendo de la edad 
+
+// 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
+
+// 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
+
+// switch
+
+// 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+
+// 9. Usa un switch para hacer de nuevo el ejercicio 6
+
+// 10. Usa un switch para hacer de nuevo el ejercicio 7
