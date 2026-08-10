ecbfaae6e0f63eed49cf82cdc2c7978028d4440d
cristianjmnz <cristianjimenez9595@gmail.com>
2026-08-10 10:49:17 +0200

    feat: loops + ejercicios

---
+++ javascript/13-loops.js
@@ -0,0 +1,14 @@
+// Loops o bucles
+
+// for
+
+for (let i = 0; i < 5; i++) {
+    console.log(`Hola ${i}`)
+};
+
+const lista_compra = ["manzana","pera","sandia","platano"];
+
+for (let i = 0; i < lista_compra.length; i++) {
+    console.log(`* ${lista_compra[i]}`)
+};
+
