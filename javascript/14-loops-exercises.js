ac8b9ddaafc18bb3acf435148497a8b7c0b3aebe
cristianjmnz <cristianjimenez9595@gmail.com>
2026-08-16 14:11:22 +0200

    feat: ejercicios hasta el 7 + primeros pasos html

--- javascript/14-loops-exercises.js
+++ javascript/14-loops-exercises.js
@@ -6,21 +6,52 @@ for (let i = 1; i <= 20; i++){
 };
 
 // 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
-for (let i = 1; i <=100; i++){
-    let suma = 0;
-    suma += i;
-    console.log(suma);
+let sumaTotal = 0;
+for (let i = 1; i <= 100; i++){
+    sumaTotal += i;
 }
+console.log(sumaTotal);
 
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
+for (let num = 1; num <= 50; num++){
+	if(num % 2 == 0){
+        console.log(num);
+    }
+	
+}
 
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
+name = ["Cristian","Susana","Freya","Jazmine"];
+
+for(let i = 0; i < name.length; i++){
+    console.log(name[i]);
+	
+}
 
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
+let texto = "Hola Cristian";
+let contador = 0;
+
+for (let i = 0; i < texto.length; i++){
+    if ("aeiouAEIOU".includes(texto[i])){
+        contador++;
+    }
+}
+
+console.log(`Número de vocales: ${contador}`);
 
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
+let numeros = [1, 2, 3, 4, 5];
+let producto = 1;
+for (let i = 0; i < numeros.length; i++){
+    producto *= numeros[i];
+    console.log(producto);
+}
 
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+for (let i = 1; i <= 10; i++){
+    console.log(`5 x ${i} = ${5 * i}`);
+}
 
 // 8. Usa un bucle para invertir una cadena de texto
 
