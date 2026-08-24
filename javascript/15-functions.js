6cded1d6a08c8423ac37e01450d34cabe1415a55
cristianjmnz <cristianjimenez9595@gmail.com>
2026-08-24 18:10:41 +0200

    feat: funciones

--- javascript/15-functions.js
+++ javascript/15-functions.js
@@ -6,15 +6,107 @@ function myFunc() {
     console.log("¡Hola, función!")
 }
 
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 5; i++) {
     myFunc()
 }
 
 // Con parámetros
 
-let name = "Cristian";
 function myFuncWithParams(name) {
     console.log(`¡Hola, ${name}!`)
 }
 
-myFuncWithParams(name);
+myFuncWithParams("Brais")
+myFuncWithParams("MoureDev")
+
+// Funciones anónimas
+
+const myFunc2 = function (name) {
+    console.log(`¡Hola, ${name}!`)
+}
+
+myFunc2("Brais Moure")
+
+// Arrow functions
+
+const myFunc3 = (name) => {
+    console.log(`¡Hola, ${name}!`)
+}
+
+const myFunc4 = (name) => console.log(`¡Hola, ${name}!`)
+
+myFunc3("Cristian")
+myFunc4("Jimenez Sanchez")
+
+// Parámetros
+
+function sum(a, b) {
+    console.log(a + b)
+}
+
+sum(5, 10)
+sum(5)
+sum()
+
+function defaultSum(a = 0, b = 0) {
+    console.log(a + b)
+}
+
+// Por defecto
+
+defaultSum()
+defaultSum(5)
+defaultSum(5, 10)
+defaultSum(undefined, 5)
+
+// Retorno de valores
+
+function mult(a, b) {
+    return a * b
+}
+
+let result = mult(5, 10)
+console.log(result)
+
+// Funciones anidadas
+
+function extern() {
+    console.log("Función externa")
+    function intern() {
+        console.log("Función interna")
+    }
+    intern()
+}
+
+extern()
+// intern() Error: fuera del scope
+
+// Funciones de orden superior
+
+function applyFunc(func, param) {
+    func(param)
+}
+
+applyFunc(myFunc4, "función de orden superior")
+
+// forEach
+
+const myArray = [1, 2, 3, 4]
+
+const mySet = new Set(["Cristian", "jmnz", "sajicri", 30, true, "cristianjmnz@cristiandev.com"])
+
+const myMap = new Map([
+    ["name", "Cristian"],
+    ["email", "cristianjmnz@cristiandev.com"],
+    ["age", 30]
+])
+
+myArray.forEach(function (value) {
+    console.log(value)
+})
+
+myArray.forEach((value) => console.log(value))
+
+mySet.forEach((value) => console.log(value))
+
+myMap.forEach((value) => console.log(value))
