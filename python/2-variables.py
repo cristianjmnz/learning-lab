ce9142ea3aa6e1aaf7b38cf89762abcd51522bb8
cristianjmnz <cristianjimenez9595@gmail.com>
2026-09-01 18:46:13 +0200

    feat: variables en python

---
+++ python/2-variables.py
@@ -0,0 +1,13 @@
+# Crear variables es como poner cosas en cajas con etiqueta
+nombre = "Cristian"
+edad = 30
+altura = 1.70
+es_estudiante = True
+
+# Leer variables: usarlas por su nombre
+print(nombre)          # Muestra: Cristian
+print(edad)            # Muestra: 30
+print(es_estudiante)   # Muestra: True
+
+# Usar variables dentro de texto con f-strings
+print(f"Me llamo {nombre}, tengo {edad} años y mido {altura}m")
